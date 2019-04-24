bf42c4afae65bf14237142f6d023415702964454
Jatos Detking <sashko125@gmail.com>
2019-04-24 21:54:01 +0300

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palendrom = Console.ReadLine();
+            Console.WriteLine(Palendrom(palendrom));
+        }
+
+        private static bool Palendrom(string palendrom)
+        {
+            int start = 0;
+            int end = palendrom.Length - 1;
+            for (int i = 0; i < palendrom.Length / 2; i++)
+            {
+                if (palendrom[start] != palendrom[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
